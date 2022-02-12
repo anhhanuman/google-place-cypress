46298be43a6426b9d253e6aa5beda7554ddfefab
anhhanuman <maivuonganh230489@gmail.com>
2022-02-12 14:13:22 +0700

    cypress file

---
+++ cypress/integration/google-place-api.spec.ts
@@ -0,0 +1,8 @@
+describe('Google Place API', () => {
+    context('Find place API', () => {
+        it('returns proper result', () => {
+            cy.visit('https://google.com')
+        })
+    })
+
+})
